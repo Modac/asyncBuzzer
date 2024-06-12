ebb8b66842b8e17465ac0be89d2521aabb98c307
Modac <modac@gmx.net>
2024-06-12 18:10:55 +0200

    Update MelodyRepeat example

--- examples/MelodyRepeat/MelodyRepeat.ino
+++ examples/MelodyRepeat/MelodyRepeat.ino
@@ -18,41 +18,32 @@ const int BUZZER_PIN = 3;
 
 asyncBuzzer buzzer(BUZZER_PIN); // create asyncBuzzer object that attach to a pin;
 
-// notes in the melody:
-int melody[] = {
-  NOTE_E5, NOTE_E5, NOTE_E5,
-  NOTE_E5, NOTE_E5, NOTE_E5,
-  NOTE_E5, NOTE_G5, NOTE_C5, NOTE_D5,
-  NOTE_E5,
-  NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5,
-  NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5,
-  NOTE_E5, NOTE_D5, NOTE_D5, NOTE_E5,
-  NOTE_D5, NOTE_G5
-};
-
-// note durations: 4 = quarter note, 8 = eighth note, etc, also called tempo:
-int noteDurations[] = {
-  8, 8, 4,
-  8, 8, 4,
-  8, 8, 8, 8,
-  2,
-  8, 8, 8, 8,
-  8, 8, 8, 16, 16,
-  8, 8, 8, 8,
-  4, 4
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+asyncBuzzer::Note melody[] = {
+  {NOTE_E5,8}, {NOTE_E5,8}, {NOTE_E5,4},
+  {NOTE_E5,8}, {NOTE_E5,8}, {NOTE_E5,4},
+  {NOTE_E5,8}, {NOTE_G5,8}, {NOTE_C5,8}, {NOTE_D5,8},
+  {NOTE_E5,2},
+  {NOTE_F5,8}, {NOTE_F5,8}, {NOTE_F5,8}, {NOTE_F5,8},
+  {NOTE_F5,8}, {NOTE_E5,8}, {NOTE_E5,8}, {NOTE_E5,16}, {NOTE_E5,16},
+  {NOTE_E5,8}, {NOTE_D5,8}, {NOTE_D5,8}, {NOTE_E5,8},
+  {NOTE_D5,4}, {NOTE_G5,4}
 };
 
 int noteLength;
 
 void setup() {
   Serial.begin(9600);
-  noteLength = sizeof(noteDurations) / sizeof(int);
+  noteLength = sizeof(melody) / sizeof(melody[0]);
 }
 
 void loop() {
   buzzer.loop(); // MUST call the buzzer.loop() function in loop()
 
   if (buzzer.getState() == BUZZER_IDLE) { // if stopped
-    buzzer.playMelody(melody, noteDurations, noteLength); // playing
+    buzzer.playMelody(melody, 240, noteLength); // playing
   }
 }
